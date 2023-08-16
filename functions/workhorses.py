4e3649ccaa472dee237610fa68612037e6608630
kreicer <42173747+kreicer@users.noreply.github.com>
2023-08-16 23:09:46 +0300

    fix typo

--- functions/workhorses.py
+++ functions/workhorses.py
@@ -81,7 +81,7 @@ def dice_roll(throws, edge):
     if edge in edge_valid:
         dice_edge_counter.labels(edge)
         dice_edge_counter.labels(edge).inc(throws)
-        dice_edge_counter.labels("all").inc(throws)
+    dice_edge_counter.labels("all").inc(throws)
     return dice_roll_result
 
 
