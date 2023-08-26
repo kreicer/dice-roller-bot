1fddc0ba96fd50b5b946e8dfcff8a8e5ce6b14e7
NovaHorizon <cpu.mage@gmail.com>
2023-08-26 04:21:28 -0600

    Add COD and WOD dice systems

--- models/regexp.py
+++ models/regexp.py
@@ -1,6 +1,6 @@
 parsing_regexp = r'^(?P<mod>[+-]*)?' \
                  r'(?P<throws>\d*)?' \
-                 r'(?P<delimiter>d|$)' \
+                 r'(?P<delimiter>cod|wod|d|$)' \
                  r'(?P<edge>\d*|[fF]$)' \
                  r'(?P<separator>/|$)' \
                  r'(?P<postfix>[a-zA-Z]*)?' \
