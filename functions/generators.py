7121b6844ac4a98ea0da2b044d93bdef19fe327e
kreicer <42173747+kreicer@users.noreply.github.com>
2023-10-26 03:12:32 +0300

    add some extra texts and change hello

--- functions/generators.py
+++ functions/generators.py
@@ -1,13 +1,15 @@
 from functions.checks import check_postfix_is_right_and_available, check_action_is_right_and_available
 from functions.colorizer import Colorizer
 from functions.config import bot_name, bot_version, dev_name, bot_shards
+from lang.EN.texts import command_shortcut_text, command_prefix_text, command_action_text, command_postfix_text
 from models.actions import actions
 from models.postfixes import postfixes
 from models.commands import cmds
 
 
 def generate_postfix_short_output():
-    output = f"<green>POSTFIXES<end>\n\n"
+    output = f"<green>POSTFIXES<end>\n"
+    output += command_postfix_text + "\n\n"
     for postfix in postfixes:
         if postfixes[postfix]["enabled"]:
             output += f"  <green>{postfix: <7}<end>{postfixes[postfix]['shorty']}\n"
@@ -33,7 +35,8 @@ def generate_postfix_help(postfix):
 
 
 def generate_action_short_output():
-    output = f"<green>ACTIONS<end>\n\n"
+    output = f"<green>ACTIONS<end>\n"
+    output += command_action_text + "\n\n"
     for action in actions:
         if actions[action]["enabled"]:
             output += f"  <green>{action: <14}<end>{actions[action]['shorty']}\n"
@@ -62,7 +65,8 @@ def generate_joke_output(joke_id, joke_text):
 
 
 def generate_prefix_output(prefix, text):
-    output = f"<green>PREFIX <yellow>(ADMIN ACTION)<end>\n\n" \
+    output = f"<green>PREFIX <yellow>(ADMIN ACTION)<end>\n" \
+             f"{command_prefix_text}\n\n" \
              f"{text} - <blue>{prefix}<end>"
     output = Colorizer(output).colorize()
     return output
@@ -104,7 +108,8 @@ def generate_commands_help(command):
 
 
 def generate_shortcut_output(shortcuts, number, limit):
-    output = f"<green>SHORTCUTS {number}/{limit} <yellow>(ADMIN ACTION)<end>\n\n"
+    output = f"<green>SHORTCUTS {number}/{limit} <yellow>(ADMIN ACTION)<end>\n"
+    output += command_shortcut_text + "\n\n"
     for shortcut, dice in shortcuts:
         output += f"  <green>{shortcut: <12}<end> <blue>{dice}<end>\n"
     output += "\n"
@@ -114,7 +119,8 @@ def generate_shortcut_output(shortcuts, number, limit):
 
 
 def generate_shortcut_empty_output():
-    output = f"<green>SHORTCUTS <yellow>(ADMIN ACTION)<end>\n\n"
+    output = f"<green>SHORTCUTS <yellow>(ADMIN ACTION)<end>\n"
+    output += command_shortcut_text + "\n\n"
     output += f"There are no shortcuts on your server yet."
     output = Colorizer(output).colorize()
     return output
--- lang/EN/texts.py
+++ lang/EN/texts.py
@@ -1,23 +1,37 @@
-command_hello_text = f"Hello, friend!\n\n" \
-             f"My name is <blue>Dice Roller<end>. I will be your guide in this awesome adventure. " \
-             f"First of all, my core functionality - roll any dice you can image. " \
-             f"And be your useful helper on this way, of course. " \
-             f"I recommend start from something simple... Make a <green>d20<end> dice roll!\n\n" \
-             f"You can roll a simple dice, complex dice with multipliers and also with Postfixes! " \
-             f"Let me show you some examples...\n\n" \
-             f"Your command should start from <green>slash (/)<end>, local bot <green>prefix<end> " \
-             f"or bot <green>mention<end>. " \
-             f"Next part - command name. In our case it will be <green>\"roll\"<end>. " \
-             f"Yep, whitespace next. " \
-             f"And from this moment only you decide what you want to roll. " \
-             f"Dice may be simple like <green>2d20<end>  or complex like <green>3d8+d4-1<end>. " \
-             f"Dice may contain <green>Postfix<end>. I will provide this as example - <green>4d8/dl:2<end>. " \
-             f"Best part here - you can combine any dice and roll more than one dice per command. " \
-             f"Like this - <green>/roll 3d6+d4 3d20/dh 2d20+2d4/dl:1-1<end>. " \
-             f"You can get list of postfixes with command <green>/postfix<end>."
+command_hello_text = "Hello, my friend!\n\n" \
+                     "I'm <blue>Dice Roller<end>, and I can roll any type of dice you can imagine:\n" \
+                     "– Basic dice of all kinds, in any quantity: d20, 3d6, 8d4, and so on.\n" \
+                     "– Dice with modifiers: d20+5, 3d8+d4-1, and so forth.\n" \
+                     "– Dice with Postfixes (extra rules): " \
+                     "For example, 2d20/kl will roll 2d20s and keep only the highest result, " \
+                     "4d6/dl will roll 4d6 and drop the lowest result, and so on. " \
+                     "To view the complete list of Postfixes, use the command <green>/postfix<end>.\n\n" \
+                     "The base syntax of my command is a slash (/) + roll + space + dice:\n" \
+                     "<green>/roll d20<end>\n\n" \
+                     "You can combine dice as you wish and use several of them with a single command, like this:\n" \
+                     "<green>/roll 3d6+d4 3d20/dh 2d20+2d4/dl:1-1<end>\n\n" \
+                     "You can set shortcuts for your favorite rolls with the command " \
+                     "<green>/shortcut<end> (server admin only).\n\n" \
+                     "To change the local prefix, use the command <green>/prefix<end> (server admin only). " \
+                     "Also, you can initiate a command by mentioning me:\n" \
+                     "<green>@DiceRoller roll d20<end>\n\n" \
+                     "If you come across a message containing a proper dice sequence, you can right-click it, choose" \
+                     " \"Apps,\" and then hit <green>\"Text to Roll\"<end>. For example, if the DM sent a message:" \
+                     "\"To roll your stat, you can use the 4d6/dl\", you can simply right-click -> " \
+                     "Apps -> Text to Roll, and I will roll 4d6/dl; no need to retype it!\n\n" \
+                     "Enjoy your adventure with the power of the Dice Roller – " \
+                     "and brighten your day with <green>/joke<end> 😉\n\n" \
+                     "P.S. to quickly see a full list of available commands, use <green>/help<end>."
 
 command_roll_parameter = "Place dice here, split with whitespace"
 
 command_prefix_output_cur = "Current guild prefix value"
 command_prefix_output_default = "Guild prefix value was restored to default"
 command_prefix_output_new = "New guild prefix value"
+command_prefix_text = "Starter of the bot commands, if you don't use slash commands or mention."
+
+command_postfix_text = "Special part of dice that add extra rules to your roll."
+
+command_shortcut_text = "Words equal to dice sequences that allow you to roll your favorites dice easy way."
+
+command_action_text = "Special part of dice that add extra actions or elements to your roll."
