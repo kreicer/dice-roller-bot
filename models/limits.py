bd9f461cb84ea537fc92a5e92aadc283969d60bc
kreicer <42173747+kreicer@users.noreply.github.com>
2023-11-08 23:11:36 +0300

    clear old limits

--- models/limits.py
+++ models/limits.py
@@ -2,9 +2,7 @@
 dice_limit = 5
 group_limit = 12
 edge_limit = 1000000
-prefix_limit = 3
 roll_limit = 50
 modifier_limit = 1000000
-joke_limit = 250
 visual_dice_label_limit = 40
 shortcuts_limit = 10
